935df07ddc17e5968050935adb5fdab58b596acc
Credit First <credit.first@gunthy.org>
2024-03-22 15:36:11 +1000

    Add config.php file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$config = array(
+    "database" => array(
+        "default" => "mysql",
+
+        "mysql" => array(
+            "host"      => "127.0.0.1",
+            "username"  => "root",
+            "password"  => "",
+            "db_name"   => "mini_backend"
+        )
+    )
+);
