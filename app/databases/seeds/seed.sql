f4c6985622431582a9141ddf20f0228ac2d7147d
Credit First <credit.first@gunthy.org>
2024-03-25 15:27:35 +1000

    Add a seed for temp data

---
+++ app/databases/seeds/seed.sql
@@ -0,0 +1,56 @@
+-- ----------------------------
+-- Records of box_subscribers
+-- ----------------------------
+INSERT INTO `box_subscribers` VALUES (1, 1, 1);
+INSERT INTO `box_subscribers` VALUES (2, 2, 1);
+INSERT INTO `box_subscribers` VALUES (3, 3, 2);
+INSERT INTO `box_subscribers` VALUES (4, 4, 2);
+INSERT INTO `box_subscribers` VALUES (5, 5, 2);
+
+-- ----------------------------
+-- Records of boxies
+-- ----------------------------
+INSERT INTO `boxies` VALUES (1, 'Orchard Tower', 1, 1);
+INSERT INTO `boxies` VALUES (2, 'United Square', 2, 1);
+INSERT INTO `boxies` VALUES (3, 'Thompson Plaza', 3, 1);
+INSERT INTO `boxies` VALUES (4, 'Peranakan Place', 4, 1);
+INSERT INTO `boxies` VALUES (5, 'Marina Boulevard', 5, 1);
+
+-- ----------------------------
+-- Records of prayers
+-- ----------------------------
+INSERT INTO `prayers` VALUES (1, 'JHR01');
+INSERT INTO `prayers` VALUES (2, 'JHR02');
+INSERT INTO `prayers` VALUES (3, 'JHR03');
+INSERT INTO `prayers` VALUES (4, 'JHR04');
+INSERT INTO `prayers` VALUES (5, 'KDH01');
+INSERT INTO `prayers` VALUES (6, 'KDH02');
+INSERT INTO `prayers` VALUES (7, 'KDH03');
+INSERT INTO `prayers` VALUES (8, 'KDH04');
+INSERT INTO `prayers` VALUES (9, 'KDH05');
+INSERT INTO `prayers` VALUES (10, 'KDH06');
+INSERT INTO `prayers` VALUES (11, 'KDH07');
+INSERT INTO `prayers` VALUES (12, 'KTN01');
+INSERT INTO `prayers` VALUES (13, 'KTN03');
+INSERT INTO `prayers` VALUES (14, 'MLK01');
+INSERT INTO `prayers` VALUES (15, 'NGS01');
+INSERT INTO `prayers` VALUES (16, 'NGS02');
+
+-- ----------------------------
+-- Records of schedules
+-- ----------------------------
+
+-- ----------------------------
+-- Records of songs
+-- ----------------------------
+INSERT INTO `songs` VALUES (1, 'song1', 1, '');
+INSERT INTO `songs` VALUES (2, 'song2', 2, '');
+INSERT INTO `songs` VALUES (3, 'song3', 3, '');
+INSERT INTO `songs` VALUES (4, 'song4', 4, '');
+INSERT INTO `songs` VALUES (5, 'song5', 5, '');
+
+-- ----------------------------
+-- Records of subscribers
+-- ----------------------------
+INSERT INTO `subscribers` VALUES (1, 'The Café');
+INSERT INTO `subscribers` VALUES (2, 'My Restaurant');
