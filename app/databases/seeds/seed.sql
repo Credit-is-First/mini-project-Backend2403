d8a06a0c058a32dab74eb3d309623552619dfb0d
Credit First <credit.first@gunthy.org>
2024-03-25 16:14:04 +1000

    Update seed.sql file

--- app/databases/seeds/seed.sql
+++ app/databases/seeds/seed.sql
@@ -5,24 +5,6 @@ INSERT INTO `subscribers` VALUES (1, 'The Café');
 INSERT INTO `subscribers` VALUES (2, 'My Restaurant');
 
 -- ----------------------------
--- Records of boxes
--- ----------------------------
-INSERT INTO `boxes` VALUES (1, 'Orchard Tower', 1, 1);
-INSERT INTO `boxes` VALUES (2, 'United Square', 2, 1);
-INSERT INTO `boxes` VALUES (3, 'Thompson Plaza', 3, 1);
-INSERT INTO `boxes` VALUES (4, 'Peranakan Place', 4, 1);
-INSERT INTO `boxes` VALUES (5, 'Marina Boulevard', 5, 1);
-
--- ----------------------------
--- Records of box_subscribers
--- ----------------------------
-INSERT INTO `box_subscribers` VALUES (1, 1, 1);
-INSERT INTO `box_subscribers` VALUES (2, 2, 1);
-INSERT INTO `box_subscribers` VALUES (3, 3, 2);
-INSERT INTO `box_subscribers` VALUES (4, 4, 2);
-INSERT INTO `box_subscribers` VALUES (5, 5, 2);
-
--- ----------------------------
 -- Records of prayers
 -- ----------------------------
 INSERT INTO `prayers` VALUES (1, 'JHR01');
@@ -41,6 +23,24 @@ INSERT INTO `prayers` VALUES (13, 'KTN03');
 INSERT INTO `prayers` VALUES (14, 'MLK01');
 INSERT INTO `prayers` VALUES (15, 'NGS01');
 INSERT INTO `prayers` VALUES (16, 'NGS02');
+
+-- ----------------------------
+-- Records of boxes
+-- ----------------------------
+INSERT INTO `boxes` VALUES (1, 'Orchard Tower', 1, 1);
+INSERT INTO `boxes` VALUES (2, 'United Square', 2, 1);
+INSERT INTO `boxes` VALUES (3, 'Thompson Plaza', 3, 1);
+INSERT INTO `boxes` VALUES (4, 'Peranakan Place', 4, 1);
+INSERT INTO `boxes` VALUES (5, 'Marina Boulevard', 5, 1);
+
+-- ----------------------------
+-- Records of box_subscribers
+-- ----------------------------
+INSERT INTO `box_subscribers` VALUES (1, 1, 1);
+INSERT INTO `box_subscribers` VALUES (2, 2, 1);
+INSERT INTO `box_subscribers` VALUES (3, 3, 2);
+INSERT INTO `box_subscribers` VALUES (4, 4, 2);
+INSERT INTO `box_subscribers` VALUES (5, 5, 2);
 
 -- ----------------------------
 -- Records of songs
